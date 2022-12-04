8ac70ef610dfb55238b94e6d96af6aaf714c0f64
Maria <mgashenina@gmail.com>
2022-12-04 10:39:59 +0300

    homework_2_1

---
+++ homework_2_1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a % 100;
+int c = b / 10;
+Console.WriteLine("Вторая цифра введенного числа: " + c);
