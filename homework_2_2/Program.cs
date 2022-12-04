6d793ade98dbf6d8e66dc078b5b3415818ca5df6
Maria <mgashenina@gmail.com>
2022-12-04 11:52:08 +0300

    homework_2_2

---
+++ homework_2_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N / 100 == 0)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    string str = Convert.ToString(N);
+    Console.WriteLine("Третья цифра числа: " + str[2]);
+}
