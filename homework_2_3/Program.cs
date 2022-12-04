f3e6603ea414ed464cffebcec8a44ec97c4292a3
Maria <mgashenina@gmail.com>
2022-12-04 19:34:55 +0300

    homework_2_3

---
+++ homework_2_3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if(N % 7 == 0)
+{
+    if(N % 23 == 0)
+    {
+        Console.Write(N + " кратно одновременно 7 и 23");
+    }    
+    else
+    {
+        Console.Write(N + " некратно одновременно 7 и 23");
+    }
+}
+else
+{
+    Console.Write(N + " некратно одновременно 7 и 23");
+}
